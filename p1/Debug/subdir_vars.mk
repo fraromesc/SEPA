08d962afe0d4e60194b8a93c311149e9a2e4bd69
fraromesc <fraromesc@gmail.com>
2022-10-03 17:54:48 +0200

    practica 1 terminada

---
+++ p1/Debug/subdir_vars.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c1294ncpdt.cmd 
+
+LIB_SRCS += \
+C:/Users/jwyx/OneDrive/Escritorio/SEPA/TivaWare_C_Series-2.1.3.156/driverlib/ccs/Debug/driverlib.lib 
+
+C_SRCS += \
+../ejercicio2.c \
+../tm4c1294ncpdt_startup_ccs.c 
+
+C_DEPS += \
+./ejercicio2.d \
+./tm4c1294ncpdt_startup_ccs.d 
+
+OBJS += \
+./ejercicio2.obj \
+./tm4c1294ncpdt_startup_ccs.obj 
+
+OBJS__QUOTED += \
+"ejercicio2.obj" \
+"tm4c1294ncpdt_startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"ejercicio2.d" \
+"tm4c1294ncpdt_startup_ccs.d" 
+
+C_SRCS__QUOTED += \
+"../ejercicio2.c" \
+"../tm4c1294ncpdt_startup_ccs.c" 
+
+
